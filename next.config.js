c3269e722ba6c481f5d41eeff78e333fced1d101
Mickael Marques <mickaelpombal@gmail.com>
2020-05-28 14:24:25 +0100

    Added Apollo lib for communication with the server

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-const withTM = require("next-transpile-modules")(["bs-platform"]);
+require("dotenv").config();
+
+const withTM = require("next-transpile-modules")([
+  "bs-platform",
+  "reason-apollo-hooks",
+]);
 
 module.exports = withTM({
   pageExtensions: ["jsx", "js", "bs.js"],
   env: {
-    analytics: "UA-166787685-1",
+    analytics: process.env.GA_ID,
+    SERVER_URL: process.env.SERVER_URL,
+    X_HASURA_ADMIN_SECRET: process.env.X_HASURA_ADMIN_SECRET,
   },
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
 import "../styles/global.css";
+import App from "next/app";
 import { make as Page } from "../components/Page.bs";
+import { withApollo } from "../utils/withApollo";
+import { ApolloProvider } from "@apollo/react-common";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
-  );
+class MySiteApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps, apolloClient } = this.props;
+
+    return (
+      <ApolloProvider client={apolloClient}>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ApolloProvider>
+    );
+  }
 }
+
+export default withApollo(MySiteApp);
