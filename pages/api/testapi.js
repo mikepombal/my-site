c972bcdcd58b71fe712382da3ed86bd226bff6dc
Mickael Marques <mickaelpombal@gmail.com>
2020-06-11 17:31:45 +0100

    Adding logs to see if they are visible in Vercel

--- pages/api/testapi.js
+++ pages/api/testapi.js
@@ -9,30 +9,33 @@ query getUser($uuid: uuid!){
 }
 `;
 
-// const execute = async (variables) => {
-//   const fetchResponse = await fetch(
-//     "https://mikepombal.herokuapp.com/v1/graphql",
-//     {
-//       method: "POST",
-//       body: JSON.stringify({
-//         query: HASURA_OPERATION,
-//         variables,
-//       }),
-//     }
-//   );
-//   const data = await fetchResponse.json();
-//   console.log("DEBUG: ", data);
-//   return data;
-// };
+const execute = async (variables) => {
+    console.log("starting execute function: ", variables);
+  const fetchResponse = await fetch(
+    "https://mikepombal.herokuapp.com/v1/graphql",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query: HASURA_OPERATION,
+        variables,
+      }),
+    }
+  );
+  console.log("cool, now getting the data: ", fetchResponse);
+  const data = await fetchResponse.json();
+  console.log("DEBUG: ", data);
+  return data;
+};
 
 export default function handler(req, res) {
-  //   const { uuid } = req.body.input;
+    console.log("Welcome to testapi: ", req);
+    const { uuid } = req.body.input;
 
-  //   const { data, errors } = await execute({ uuid });
+    const { data, errors } = await execute({ uuid });
 
-  //   if (errors) {
-  //     return res.status(400).json(errors[0])
-  //   }
+    if (errors) {
+      return res.status(400).json(errors[0])
+    }
 
   res.status(200).json({ accessToken: "data.users_by_pk.username" });
 }
