29d872d742cdb5cc9be5bed888616de341e0ba55
Mickael Marques <mickaelpombal@gmail.com>
2020-06-11 15:56:49 +0100

    testing running a hasura query from the api function

--- pages/api/testapi.js
+++ pages/api/testapi.js
@@ -1,3 +1,39 @@
+const fetch = require("node-fetch");
+
+const HASURA_OPERATION = `
+query getUser($uuid: uuid!){
+  users_by_pk(token: $uuid) {
+    token
+    username
+  }
+}
+`;
+
+const execute = async (variables) => {
+  const fetchResponse = await fetch(
+    "https://mikepombal.herokuapp.com/v1/graphql",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query: HASURA_OPERATION,
+        variables,
+      }),
+    }
+  );
+  const data = await fetchResponse.json();
+  console.log("DEBUG: ", data);
+  return data;
+};
+
 export default function handler(req, res) {
-  res.status(200).json({ accessToken: "ThisWillBeTheToken" });
+
+    const { uuid } = req.body.input;
+
+    const { data, errors } = await execute({ uuid });
+
+    if (errors) {
+      return res.status(400).json(errors[0])
+    }
+  
+  res.status(200).json({ accessToken: data.users_by_pk.username });
 }
