338705d2a6d40416a63be26ecb0c0559bf77c025
Mickael Marques <mickaelpombal@gmail.com>
2020-06-11 17:11:11 +0100

    is it node fetch?

--- pages/api/testapi.js
+++ pages/api/testapi.js
@@ -9,32 +9,30 @@ query getUser($uuid: uuid!){
 }
 `;
 
-const execute = async (variables) => {
-  const fetchResponse = await fetch(
-    "https://mikepombal.herokuapp.com/v1/graphql",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        query: HASURA_OPERATION,
-        variables,
-      }),
-    }
-  );
-  const data = await fetchResponse.json();
-  console.log("DEBUG: ", data);
-  return data;
-};
+// const execute = async (variables) => {
+//   const fetchResponse = await fetch(
+//     "https://mikepombal.herokuapp.com/v1/graphql",
+//     {
+//       method: "POST",
+//       body: JSON.stringify({
+//         query: HASURA_OPERATION,
+//         variables,
+//       }),
+//     }
+//   );
+//   const data = await fetchResponse.json();
+//   console.log("DEBUG: ", data);
+//   return data;
+// };
 
 export default function handler(req, res) {
-  const { uuid } = req.body.input;
+  //   const { uuid } = req.body.input;
 
-  const { data, errors } = await execute({ uuid });
+  //   const { data, errors } = await execute({ uuid });
 
-  if (errors) {
-    return res.status(400).json(errors[0])
-  }
+  //   if (errors) {
+  //     return res.status(400).json(errors[0])
+  //   }
 
-  res
-    .status(200)
-    .json({ accessToken: "data.users_by_pk.username" });
+  res.status(200).json({ accessToken: "data.users_by_pk.username" });
 }
